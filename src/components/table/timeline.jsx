7acc1601bed05cc55dfa50ba35d6ea944c5d816b
suntzu93 <thanhlv93@gmail.com>
2022-10-14 15:44:40 +0700

    Add timeline and update port

---
+++ src/components/table/timeline.jsx
@@ -0,0 +1,59 @@
+import React, { useMemo } from "react";
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import Link from "@mui/material/Link";
+import { ReactComponent as ShareLink } from "../../assets/link.svg";
+import styles from "./styles.module.css";
+
+import * as Data from "../../pages/data";
+
+export default function TransactionTimeline(transactions) {
+    const rowData = useMemo(() => transactions, [transactions]);
+    const transactionLength = rowData.transactions.length;
+    return (
+        <React.Fragment>
+            <Timeline>
+                {rowData.transactions.map((transaction, index) => (
+                    <TimelineItem>
+                        <TimelineOppositeContent color="text.secondary">
+                            {Data.calculateTimeMoment(transaction.timestamp)}
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                            <TimelineDot />
+                            {
+                                index != transactionLength - 1 ? <TimelineConnector /> : ""
+                            }
+                        </TimelineSeparator>
+                        <TimelineContent>
+                            <Link
+                                target="_blank"
+                                underline="hover"
+                                href={transaction.txHash}
+                                className={styles.link}
+                            >
+                                {transaction.description}
+                            </Link>
+                            <ShareLink />
+
+                            <span className={styles.bySpan}>by</span>
+                            <Link
+                                target="_blank"
+                                underline="hover"
+                                href={transaction.from}
+                                className={styles.by_link}
+                            >
+                                {Data.formatString(transaction.from)}
+                            </Link>
+                            <ShareLink />
+                        </TimelineContent>
+                    </TimelineItem>
+                ))}
+            </Timeline>
+        </React.Fragment>
+    );
+}
