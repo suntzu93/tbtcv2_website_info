4e8a9fe1002ed966511dae2a6e623ecfc969f8b6
suntzu93 <thanhlv93@gmail.com>
2023-07-05 15:38:00 +0700

    fix search redemption

--- src/pages/home/redeem.jsx
+++ src/pages/home/redeem.jsx
@@ -1,74 +1,86 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import * as Data from "../data";
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 import RedeemTable from "../../components/table/redeem";
 
-const RedeemsPage = ({network, isSearch, searchInput}) => {
-    const [pageData, setPageData] = useState({
-        rowData: [],
-        isLoading: false,
-        totalPassengers: 0,
-    });
+const RedeemsPage = ({ network, isSearch, searchInput }) => {
+  const [pageData, setPageData] = useState({
+    rowData: [],
+    isLoading: false,
+    totalPassengers: 0,
+  });
 
-    useEffect(() => {
-        setPageData((prevState) => ({
-            ...prevState,
-            isLoading: true
-        }));
+  useEffect(() => {
+    setPageData((prevState) => ({
+      ...prevState,
+      isLoading: true,
+    }));
 
-        Data.getRedeems(network, isSearch, searchInput).then((info) => {
-            const totalPassengers = info?.length;
-            setPageData({
-                isLoading: false,
-                rowData: Data.formatRedeems(info.redemptions),
-                tbtctoken: info.tbtctoken,
-                totalPassengers: totalPassengers
-            });
+    Data.getRedeems(network, isSearch, searchInput).then((info) => {
+      const totalPassengers = info?.length;
+      if (info?.redemptions === undefined) {
+        setPageData({
+          isLoading: false,
+          rowData: [],
+          tbtctoken: info.tbtctoken,
+          totalPassengers: 0,
         });
+      } else {
+        setPageData({
+          isLoading: false,
+          rowData: Data.formatRedeems(info.redemptions),
+          tbtctoken: info.tbtctoken,
+          totalPassengers: totalPassengers,
+        });
+      }
+    });
+  }, [network, isSearch]);
 
-    }, [network]);
-
-    return (
-        <div>
-            <div className={styles.operator_detail_header}>
-                <div className={styles.operator_detail_header_address}>
-                    {
-                        isSearch ? (
-                            <>
-                                <h4>Search : {searchInput}</h4>
-                                <span>{pageData.totalPassengers} redeems</span>
-                            </>
-                        ) : (
-                            <>
-                                <h3>Redeems</h3>
-                                <span>{pageData.totalPassengers} redeems</span></>
-                        )
-                    }
-                </div>
-                <div className={styles.operator_detail_header_value}>
-                    <div className={styles.operator_detail_header_value_item}>
-                        <div className={styles.operator_detail_header_value_item_lable}>total burned
-                        </div>
-                        <div>
-                            <div>{pageData.tbtctoken === undefined ? "loading..." : Data.formatGwei(pageData.tbtctoken?.totalBurn)}<span
-                                className={styles.span_t_token}>{" tBTC"}</span></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/*<div style={{textAlign:"center"}}>*/}
-            {/*    <h6>This function is currently being implemented.</h6>*/}
-            {/*</div>*/}
-            <div className={styles.table_content}>
-                <RedeemTable
-                    columns={Data.redeem_columns}
-                    data={pageData.rowData}
-                    isLoading={pageData.isLoading}
-                    network={network}
-                />
-            </div>
+  return (
+    <div>
+      <div className={styles.operator_detail_header}>
+        <div className={styles.operator_detail_header_address}>
+          {isSearch ? (
+            <>
+              <h4>Search : {searchInput}</h4>
+              <span>{pageData.totalPassengers} redeems</span>
+            </>
+          ) : (
+            <>
+              <h3>Redeems</h3>
+              <span>{pageData.totalPassengers} redeems</span>
+            </>
+          )}
         </div>
-    );
-}
+        <div className={styles.operator_detail_header_value}>
+          <div className={styles.operator_detail_header_value_item}>
+            <div className={styles.operator_detail_header_value_item_lable}>
+              total burned
+            </div>
+            <div>
+              <div>
+                {pageData.tbtctoken === undefined
+                  ? "loading..."
+                  : Data.formatGwei(pageData.tbtctoken?.totalBurn)}
+                <span className={styles.span_t_token}>{" tBTC"}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*<div style={{textAlign:"center"}}>*/}
+      {/*    <h6>This function is currently being implemented.</h6>*/}
+      {/*</div>*/}
+      <div className={styles.table_content}>
+        <RedeemTable
+          columns={Data.redeem_columns}
+          data={pageData.rowData}
+          isLoading={pageData.isLoading}
+          network={network}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default RedeemsPage;
